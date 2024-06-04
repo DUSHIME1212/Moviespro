a2b6e8dc192494ec10a575d61fd7f818782eea65
DUSHIME Don Aime Hosanna <118371312+DUSHIME1212@users.noreply.github.com>
2024-06-04 10:37:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=8824473926d37dbf91ff430be012d6c6&query=';
 
+getMovies(API_URL);
+
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
-getMovies(API_URL);
 
 async function getMovies(url) {
   const res = await fetch(url);
